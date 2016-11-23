d5d63211985e297f1ced81593f70b9aa58b03275
deaf.tadashi <deaf.tadashi.suzuki@gmail.com>
2016-11-23 15:22:46 +0900

    台本初版とデモ用プログラムを反映

--- src/main/java/jjug/ccc/MyBenchmark.java
+++ src/main/java/jjug/ccc/MyBenchmark.java
@@ -31,14 +31,90 @@
 
 package jjug.ccc;
 
-import org.openjdk.jmh.annotations.Benchmark;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@State(Scope.Thread)
+@Warmup(iterations=3)
+@Measurement(iterations=3)
+@Fork(2)
 public class MyBenchmark {
 
-    @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
-    }
+	private List<String> strings = new ArrayList<>(count);
+	private static final int count = 5000;
+
+	@Setup
+	public void init() {
+		strings = new ArrayList<>(count);
+		for (int i = 0; i < count; i++) {
+			strings.add("[Str" + String.format("%1$03d", i) + "]");
+		}
+	}
+	
+	@Benchmark
+	public void test1String() {
+		concat1String(strings);
+	}
+	
+	@Benchmark
+	public void test2StringConcat() {
+		concat2StringConcat(strings);
+	}
+	
+	@Benchmark
+	public void test3StringBufferConcat() {
+		concat3StringBuffer(strings);
+	}
+	
+	@Benchmark
+	public void test4StringBuilder() {
+		concat4StringBuilder(strings);
+	}
+
+	public String concat1String(List<String> strs) {
+		String strings = "";
+		for (String str : strs) {
+			strings += str;
+			strings += "\n";
+		}
+		return strings;
+	}
+
+	public String concat2StringConcat(List<String> strs) {
+		String strings = "";
+		for (String str : strs) {
+			strings = strings.concat(str);
+			strings = strings.concat("\n");
+		}
+		return strings;
+	}
+
+
+	public String concat3StringBuffer(List<String> strs) {
+		StringBuffer strings = new StringBuffer();
+		for (String str : strs) {
+			strings.append(str);
+			strings.append("\n");
+		}
+		return strings.toString();
+	}
+
+
+	public String concat4StringBuilder(List<String> strs) {
+		StringBuilder strings = new StringBuilder();
+		for (String str : strs) {
+			strings.append(str);
+			strings.append("\n");
+		}
+		return strings.toString();
+	}
 
 }
